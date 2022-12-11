c14495006ae8b213ea9ed708a9dfd84d3bd39a56
mehmetemineker <mehmetemineker92@gmail.com>
2022-12-11 18:20:56 +0300

    tailwind card test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,35 +16,26 @@ const { t } = useI18n()
 
 <template>
   <div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Password Generator
-      </a>
-    </p>
-    <p>
-      <!-- <em text-sm opacity-75>{{ t('intro.desc') }}</em> -->
+    <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+      <div>
+        <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+        </span>
+      </div>
+      <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Password Generator</h3>
+      <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        For fast, practical and more secure passwords.
+      </p>
 
-      <em>For fast, practical and more secure passwords.</em>
-    </p>
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
 
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+      <div>
+        <button btn m-3 text-sm :disabled="!name" @click="go">
+          {{ t('button.go') }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
